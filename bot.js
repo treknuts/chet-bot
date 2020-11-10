a91e7bb09a457935a4176c74e1ea1128802fe5e2
treknuts <trevor.a.knutson@ndsu.edu>
2020-11-10 12:53:37 -0600

    Added game command

--- bot.js
+++ bot.js
@@ -46,7 +46,9 @@ bot.on("message", (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   console.log(args);
   const command = args[0];
-  if (command === "@thegoddamnbar") {
+  if (command === "game") {
+    message.channel.send(`<@${"775171418459602944"}> lets play some games!`);
+  } else if (command === "@thegoddamnbar") {
     message.channel.send(`<@${"229462482337529856"}> is at the bar. :beer:`);
   } else if (command === "unpoopmypants") {
     message.channel.send(
